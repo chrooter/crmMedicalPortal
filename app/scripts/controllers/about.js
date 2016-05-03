ddcd59720d6052d9999998f613632404245eb938
xxf456123xxf <549748120@qq.com>
2016-05-03 14:15:32 +0800

    Signed-off-by: xxf456123xxf <549748120@qq.com>

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crmMedicalPortalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crmMedicalPortalApp
+ */
+angular.module('crmMedicalPortalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crmMedicalPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crmMedicalPortalApp
+ */
+angular.module('crmMedicalPortalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
