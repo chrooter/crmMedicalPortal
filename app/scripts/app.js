de5d5446bd6a139a3274616f58e242bc81215c4f
xxf456123xxf <549748120@qq.com>
2016-05-04 16:58:22 +0800

    Signed-off-by: xxf456123xxf <549748120@qq.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,18 +19,12 @@ angular
     'ngTouch'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+        var routes, setRoutes;
+        return routes = ["about"],
+        setRoutes = function (route) {
+            var config, url;
+            return url = "/" + route, config = { templateUrl: "views/" + route + ".html" }, $routeProvider.when(url, config), $routeProvider
+        }, routes.forEach(function (route) {
+            return setRoutes(route)
+        }), $routeProvider.when("/", { redirectTo: "/ui/dashboard" }).when("/404", { templateUrl: "views/pages/404.html" }).otherwise({ redirectTo: "/construction" })
   });
